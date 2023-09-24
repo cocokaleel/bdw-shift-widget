1200070deb82818d57bcefcc8b78dd64c6a261da
cocokaleel <cocokaleel@gmail.com>
2023-09-24 13:31:31 -0400

    json parsing goddamn other errors

--- src/widgets/monitors.js
+++ src/widgets/monitors.js
@@ -1,16 +1,27 @@
 import React from "react";
+import data from './monitor-info/monitors.json'
+
 
 class Monitors extends React.Component {
-    constructor(props) {
-        //don't have any props atm
-    }
-    componentDidMount() {
-        //this function is called when the component is added to the DOM
-    }
+    
+    // constructor(props) {
+    //     //don't have any props atm
+    // }
+    // componentDidMount() {
+    //     //this function is called when the component is added to the DOM
+    // }
     render() {
+        console.log("about to log:")
+        console.log(data)
         //defines the render rules
         return (<div>
             <h1>Monitors</h1>
+            <select id="monitor1">
+                {data.monitors.map((name_string)=>{return <option value={name_string}>{name_string}</option>})}
+            </select>
+            <select id="monitor2">
+                {data.monitors.map((name_string)=>{return <option value={name_string}>{name_string}</option>})}
+            </select>
         </div>)
     }
 }
